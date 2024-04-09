f693c93d2e50d506e9e5e499cd104ea34102f74e
Virtuos Digital <siddharthmiglani001@gmail.com>
2024-04-09 11:01:12 +0530

    MongoDB connection

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDB = async ()=> {
+
+    console.log(process.env.MONGODB_URI)
+    try{
+
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            console.error("MongoDB URI is not defined in .env file");
+            // process.exit(1);
+        }
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`)
+    }
+    catch(error)
+    {
+        console.log("MongoDB Connection error", error);
+        process.exit(1);
+    }
+} 
+
+export default connectDB;
