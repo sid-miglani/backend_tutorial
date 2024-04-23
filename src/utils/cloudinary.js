c89c9c166371e824a1975a412207fcb090fbed03
Siddharth Miglani <sidmig001@gmail.com>
2024-04-23 10:23:34 +0530

    User Routes 1

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+
+        if(!localFilePath) return null;
+
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        // File has been uploaded successfully
+        console.log('File hhas been uploaded successfully', response.url);
+        return response;
+    }
+    catch(error)
+    {
+        //remove the locally saved temporary file as the upload opeartion got failed
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+    
+}
+
+export {uploadOnCloudinary};
